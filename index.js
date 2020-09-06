7086d44a84bb84b0b4870006560de9570115a3ec
Kavyasree S G <kavyasreesg1997@gmail.com>
2020-09-06 12:37:03 +0530

    Committing with mouse click event listeners

---
+++ index.js
@@ -0,0 +1,43 @@
+// When the first button in HTML file is clicked, handleClick() is called
+
+var n = document.querySelectorAll(".drum").length;
+for(i=0;i<n;i++)
+{
+    document.querySelectorAll("button")[i].addEventListener("click",function handleClick(){
+        var buttonInnerHTML = this.innerHTML;
+        switch (buttonInnerHTML) {
+            case "w":
+                var audio = new Audio("sounds/snare.mp3")
+                audio.play();
+                break;
+            case "a":
+                var audio = new Audio("sounds/tom-1.mp3")
+                audio.play();
+                break;
+            case "s":
+                var audio = new Audio("sounds/tom-2.mp3")
+                audio.play();
+                break;
+            case "d":
+                var audio = new Audio("sounds/tom-3.mp3")
+                audio.play();
+                break;
+            case "j":
+                var audio = new Audio("sounds/tom-4.mp3")
+                audio.play();
+                break;
+            case "k":
+                var audio = new Audio("sounds/kick-bass.mp3")
+                audio.play();
+                break;
+            case "l":
+                var audio = new Audio("sounds/crash.mp3")
+                audio.play();
+                break;
+
+            default:
+                break;
+        }
+    });
+}
+
