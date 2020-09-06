89e016774950752696e09d6892d900fa466b11c0
Kavyasree S G <kavyasreesg1997@gmail.com>
2020-09-06 13:08:37 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,43 +1,55 @@
-// When the first button in HTML file is clicked, handleClick() is called
 
+//Detecting button clicks
 var n = document.querySelectorAll(".drum").length;
-for(i=0;i<n;i++)
-{
-    document.querySelectorAll("button")[i].addEventListener("click",function handleClick(){
+for (i = 0; i < n; i++) {
+    document.querySelectorAll("button")[i].addEventListener("click", function () {
         var buttonInnerHTML = this.innerHTML;
-        switch (buttonInnerHTML) {
-            case "w":
-                var audio = new Audio("sounds/snare.mp3")
-                audio.play();
-                break;
-            case "a":
-                var audio = new Audio("sounds/tom-1.mp3")
-                audio.play();
-                break;
-            case "s":
-                var audio = new Audio("sounds/tom-2.mp3")
-                audio.play();
-                break;
-            case "d":
-                var audio = new Audio("sounds/tom-3.mp3")
-                audio.play();
-                break;
-            case "j":
-                var audio = new Audio("sounds/tom-4.mp3")
-                audio.play();
-                break;
-            case "k":
-                var audio = new Audio("sounds/kick-bass.mp3")
-                audio.play();
-                break;
-            case "l":
-                var audio = new Audio("sounds/crash.mp3")
-                audio.play();
-                break;
+        keyPress(buttonInnerHTML);
+    }
+    );
+}
 
-            default:
-                break;
-        }
-    });
+
+//Detecting keybord clicks
+document.addEventListener("keypress", function (event) {
+    key = event.key;
+    keyPress(key);
+});
+
+
+function keyPress(key) {
+    switch (key) {
+        case "w":
+            var audio = new Audio("sounds/snare.mp3")
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio("sounds/tom-1.mp3")
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio("sounds/tom-2.mp3")
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio("sounds/tom-3.mp3")
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio("sounds/tom-4.mp3")
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio("sounds/kick-bass.mp3")
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio("sounds/crash.mp3")
+            audio.play();
+            break;
+
+        default:
+            break;
+    }
 }
 
